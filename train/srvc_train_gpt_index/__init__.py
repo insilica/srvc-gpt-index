64eca4e09ac43d5cbefc203d62bf8614e0bfd90e
John Shaffer <john@insilica.co>
2023-03-06 18:03:27 -0600

    Use gpt-3.5-turbo

--- train/srvc_train_gpt_index/__init__.py
+++ train/srvc_train_gpt_index/__init__.py
@@ -1,4 +1,5 @@
-from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from gpt_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
+from langchain import OpenAI
 import argparse
 
 def main():
@@ -8,5 +9,6 @@ def main():
   args = parser.parse_args()
 
   documents = SimpleDirectoryReader(args.source_dir, recursive=True).load_data()
-  index = GPTSimpleVectorIndex(documents)
+  llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=2048))
+  index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor)
   index.save_to_disk(args.index_file)
